bc898dc1194c5fa0c7ce3ca23fd27ddc50da49a5
ag3i <nyan76cat@gmail.com>
2022-02-06 00:22:33 +0900

    コード一部変更

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from datetime import datetime,timedelta
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-client = boto3.client('ce', region_name='us-east-1')
-dotenv_path = join(dirname(__file__), '.env')
-
 # 合計請求額を取得
-def get_cost(client) -> dict:
+def get_cost():
+
+    client = boto3.client('ce', region_name='us-east-1')
     
     start = datetime.now().replace(day=1).strftime('%F')
     end = datetime.now().strftime('%F')
@@ -23,17 +22,13 @@ def get_cost(client) -> dict:
         ]
     )
 
-    return {
+    date = {
         'start': response['ResultsByTime'][0]['TimePeriod']['Start'],
         'end': response['ResultsByTime'][0]['TimePeriod']['End'],
         'billing': response['ResultsByTime'][0]['Total']['AmortizedCost']['Amount'],
     }
-
-def calc_exchange():
-
-    date = get_cost(client)
     
-    #get exchange yen-dollar
+    #　$　⇨　￥
     exchange = requests.get('https://www.gaitameonline.com/rateaj/getrate').json()['quotes'][20]['open']
     total =  float(exchange)*float(date['billing'])
 
@@ -42,22 +37,16 @@ def calc_exchange():
     
     return f"({date['start']}から{date['end']}の利用料金は{d.group(1)}円です)"
 
-# LINEに通知
-def send_message():
-
-    load_dotenv(dotenv_path)
+def main():
+    
+    # 料金を取得
+    text = get_cost()
+    
+    # LINEに通知
     token = os.environ["LINE_token"]
-
-    headers = {
-        'Authorization':token,
-    }
-    
-    text = calc_exchange()
-    
-    files = {
-        'message': (None,text),
-    }
-    
+    headers,files = {'Authorization':token,},{'message': (None,text),}
     requests.post('https://notify-api.line.me/api/notify', headers=headers, files=files)
-    
-send_message()
+
+if __name__ == '__main__':
+    load_dotenv(join(dirname(__file__), '.env'))
+    main()
